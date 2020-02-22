2c48f64ae6dbebcfc8aab056a85cb5571af1acdf
maximivanov2004 <maxim.ivanov1711@gmail.com>
2020-02-22 22:34:48 +0300

    client 0.1 beta (added encrypt and decrypt functions)

--- client.py
+++ client.py
@@ -1,1 +1,25 @@
-# в разработке
+import requests
+
+
+class Client:
+
+    def __init__(self):
+        pass
+
+    def sendMessage(self, text):
+        pass
+
+
+def encrypt(text, closed_key):
+    open_key = []
+    for i in text:
+        open_key.append(ord(i) * closed_key)
+    return open_key
+
+
+def decrypt(open_key, closed_key):
+    message = ''
+    for i in open_key:
+        let = int(i / closed_key)
+        message += chr(let)
+    return message
