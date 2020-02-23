e4793fd572fd95a20b32cd8048809137cae7e247
maximivanov2004 <maxim.ivanov1711@gmail.com>
2020-02-23 09:41:13 +0300

    client 0.25 beta

--- client.py
+++ client.py
@@ -28,10 +28,17 @@ def decrypt(open_key, closed_key):
 print('...')
 print('Создан ли ключ? [y / n]', end=' ')
 if input().strip() == 'y':
-    pass
+    while True:
+        print('Введите закрытый ключ...')
+        try:
+            closed_key = int(input().strip())
+            break
+        except ValueError:
+            print('Попробуйте еще раз... ключ должен содержать только цифры')
+            continue
 else:
     closed_key = random.randint(10000, 1000000000)
-    print(f'Закрытый ключ создан\nclosed key: {closed_key}')
+    print(f'Закрытый ключ создан\nclosed_key: {closed_key}')
     print('Сохранить локально? [y /n]', end=' ')
     if input().strip() == 'y':
         with open('closed_key.txt', 'w') as file:
